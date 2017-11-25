b5513e865cb121c4c718ba71663071c714505af1
UITCEEC <quan.ng0anhin@yahoo.com.vn>
2017-11-25 11:08:20 +0700

    Android App
    First Code

---
+++ Android app/SmartGo/app/src/main/java/com/example/admin/smartgo/ReportActivity.java
@@ -0,0 +1,25 @@
+package com.example.admin.smartgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ReportActivity extends AppCompatActivity {
+
+    Button btnReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        btnReport = (Button) findViewById(R.id.btnReport);
+        btnReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
